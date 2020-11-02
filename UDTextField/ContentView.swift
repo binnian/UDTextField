2f2df9e5ebff222193dbdaecf96c4e0a3f3c1731
BenOnShi <shibn9510@gmail.com>
2020-11-02 10:52:34 +0800

    Initial Commit

---
+++ UDTextField/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UDTextField
+//
+//  Created by apple on 2020/11/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UDTextField/UDTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  UDTextFieldApp.swift
+//  UDTextField
+//
+//  Created by apple on 2020/11/2.
+//
+
+import SwiftUI
+
+@main
+struct UDTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
